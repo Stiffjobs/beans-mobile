6d2a6edcbf08d99c1de47f669eb4bfa923ba8c80
Stiffjobs <charleschen0306@gmail.com>
2025-01-20 00:08:06 +0800

    some updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,111 @@
+import '../global.css';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+	DarkTheme,
+	DefaultTheme,
+	Theme,
+	ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
+import '../global.css';
+import { useColorScheme } from '~/lib/useColorScheme';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { Provider as ModalProvider } from '@/state/modals';
+import { Provider as SessionProvider } from '@/state/session';
+import QueryProvider from '@/providers/QueryProvider';
+import { NAV_THEME } from '~/lib/constants';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { ModalsContainer } from '~/view/com/modals/Modal';
+import { Platform } from 'react-native';
+import { verifyInstallation } from 'nativewind';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+	// Ensure that reloading on `/modal` keeps a back button present.
+	initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
+const LIGHT_THEME: Theme = {
+	...DefaultTheme,
+	colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+	...DarkTheme,
+	colors: NAV_THEME.dark,
+};
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+	const [loaded, error] = useFonts({
+		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+		...FontAwesome.font,
+	});
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+	if (!loaded) {
+		return null;
+	}
+	return <RootLayoutNav />;
 }
-
+const useIsomorphicLayoutEffect =
+	Platform.OS === 'web' && typeof window === 'undefined'
+		? React.useEffect
+		: React.useLayoutEffect;
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+	const hasMounted = React.useRef(false);
+	const { colorScheme, isDarkColorScheme } = useColorScheme();
+	const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+	useIsomorphicLayoutEffect(() => {
+		if (hasMounted.current) {
+			return;
+		}
+
+		if (Platform.OS === 'web') {
+			// Adds the background color to the html element to prevent white background on overscroll.
+			document.documentElement.classList.add('bg-background');
+		}
+		setIsColorSchemeLoaded(true);
+		hasMounted.current = true;
+	}, []);
+	if (!isColorSchemeLoaded) {
+		return null;
+	}
+	verifyInstallation();
+
+	return (
+		<SessionProvider>
+			<QueryProvider>
+				<GestureHandlerRootView style={{ flex: 1 }}>
+					<ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+						<ModalProvider>
+							{/* <BottomSheetModalProvider> */}
+							<Slot />
+							<ModalsContainer />
+							{/* </BottomSheetModalProvider> */}
+						</ModalProvider>
+					</ThemeProvider>
+				</GestureHandlerRootView>
+			</QueryProvider>
+		</SessionProvider>
+	);
 }
