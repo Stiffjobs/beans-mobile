dd704fc52407ef4ddadae4b4c85eae83f5f69c5a
Stiffjobs <charleschen0306@gmail.com>
2025-03-06 12:15:27 +0900

    profile page

--- app/(app)/(stack)/(tabs)/profile.tsx
+++ app/(app)/(stack)/(tabs)/profile.tsx
@@ -1,6 +1,4 @@
 import { View } from 'react-native';
-import Animated, { FadeIn } from 'react-native-reanimated';
-
 import ProfileInfo from '~/components/ProfileInfo';
 import { Authenticated, Unauthenticated } from 'convex/react';
 import { Link, Stack } from 'expo-router';
@@ -8,9 +6,6 @@ import { Button } from '~/components/ui/button';
 import { Text } from '~/components/ui/text';
 import React, { useState } from 'react';
 import { Hamburger } from '~/view/com/util/Hamburger';
-import { Image } from 'expo-image';
-import { usePhotoLibraryPermission } from '~/lib/hooks/usePermissions';
-import { openCropper, openPicker } from '~/lib/media/picker';
 export default function ProfileScreen() {
 	return (
 		<View className="flex-1 items-center justify-center">
@@ -20,7 +15,7 @@ export default function ProfileScreen() {
 						headerLeft: () => <Hamburger />,
 					}}
 				/>
-				<ProfileInfo path="app/(tabs)/two.tsx" />
+				<ProfileInfo />
 			</Authenticated>
 			<Unauthenticated>
 				<Link asChild href={'/signin'}>
--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -6,11 +6,12 @@ import { useGetCurrentUser, useSignOut } from '~/state/queries/auth';
 import { Text } from './ui/text';
 import { useModalControls } from '~/state/modals';
 import { UserAvatar } from '~/view/com/util/UserAvatar';
-
-export default function EditScreenInfo({ path }: { path: string }) {
+import { ReportDialog, useReportDialogControl } from './ReportDialog';
+export default function EditScreenInfo() {
 	const currentUser = useGetCurrentUser();
 	const { openModal } = useModalControls();
 	const signOutMutation = useSignOut();
+	const reportDialogControl = useReportDialogControl();
 
 	const handleSignOut = async () => {
 		await signOutMutation.mutateAsync();
@@ -21,6 +22,9 @@ export default function EditScreenInfo({ path }: { path: string }) {
 			name: 'edit-profile',
 			user: currentUser.data!,
 		});
+	};
+	const openReportDialog = () => {
+		reportDialogControl.open();
 	};
 	return (
 		<View className="gap-4 items-center">
@@ -33,6 +37,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
 			<Button variant={'destructive'} onPress={handleSignOut}>
 				<Text className="font-bold">Sign Out</Text>
 			</Button>
+			<ReportDialog control={reportDialogControl} params={{ type: 'post' }} />
 		</View>
 	);
 }
