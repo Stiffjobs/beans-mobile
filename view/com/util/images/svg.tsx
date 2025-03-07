1cfc2635204bae2c5935ca4a2ec4e0832d875949
Stiffjobs <charleschen0306@gmail.com>
2025-03-08 02:08:08 +0900

    clean shit

--- view/com/util/images/svg.tsx
+++ view/com/util/images/svg.tsx
@@ -1,34 +1,25 @@
-import Svg, {
-	Circle,
-	Rect,
-	Path,
-	SvgProps,
-	CircleProps,
-	RectProps,
-	PathProps,
-} from 'react-native-svg';
-import { cssInterop } from 'nativewind';
-import { useMemo } from 'react';
+import Svg, { Circle, Path, CircleProps, PathProps } from "react-native-svg";
+import { cssInterop } from "nativewind";
 
 export const SvgNativeWind = cssInterop(Svg, {
-	className: {
-		target: 'style',
-		nativeStyleToProp: { width: true, height: true },
-	},
+  className: {
+    target: "style",
+    nativeStyleToProp: { width: true, height: true },
+  },
 });
 
 export const CircleNativeWind = cssInterop(Circle, {
-	className: {
-		target: 'style' as keyof CircleProps,
-		nativeStyleToProp: {
-			fill: 'fill' as keyof CircleProps,
-		},
-	},
+  className: {
+    target: "style" as keyof CircleProps,
+    nativeStyleToProp: {
+      fill: "fill" as keyof CircleProps,
+    },
+  },
 });
 
 export const PathNativeWind = cssInterop(Path, {
-	className: {
-		target: 'style' as keyof PathProps,
-		nativeStyleToProp: { fill: 'fill' as keyof PathProps },
-	},
+  className: {
+    target: "style" as keyof PathProps,
+    nativeStyleToProp: { fill: "fill" as keyof PathProps },
+  },
 });
