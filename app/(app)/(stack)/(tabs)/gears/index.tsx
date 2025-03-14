96fee75bb1dd9cb128532bfef72d1143d643fd4b
Stiffjobs <charleschen0306@gmail.com>
2025-03-14 23:36:32 +0900

    enable editting gears details

--- app/(app)/(stack)/(tabs)/gears/index.tsx
+++ app/(app)/(stack)/(tabs)/gears/index.tsx
@@ -1,12 +1,13 @@
 import { FlashList } from '@shopify/flash-list';
 import { useCallback, useState } from 'react';
-import { RefreshControl, View } from 'react-native';
+import { Pressable, RefreshControl, View } from 'react-native';
 import { FAB } from '~/components/FAB';
 import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
 import { Separator } from '~/components/ui/separator';
 import { Text } from '~/components/ui/text';
 import { H4 } from '~/components/ui/typography';
 import { GEAR_TYPE } from '~/lib/constants';
+import { GearData } from '~/lib/types';
 import { useModalControls } from '~/state/modals';
 import { useListGears } from '~/state/queries/gears';
 import { Brewer, Grinder } from '~/view/com/icons/SvgIcons';
@@ -36,10 +37,8 @@ export default function Gears() {
 					return (
 						<GearCard
 							gear={{
-								name: item.name,
+								...item,
 								type: item.type as GEAR_TYPE,
-								details: item.details,
-								settings: item.settings,
 							}}
 						/>
 					);
@@ -51,14 +50,20 @@ export default function Gears() {
 }
 
 function GearCard({ gear }: { gear: GearData }) {
+	const { openModal } = useModalControls();
+	const openEditGearModal = useCallback(() => {
+		openModal({ name: 'edit-gear', id: gear._id });
+	}, [openModal, gear._id]);
 	return (
-		<View className="p-4 justify-between flex flex-row">
-			<View className="">
-				<H4>{gear.name}</H4>
-				<Text className="text-secondary">{gear.type}</Text>
+		<Pressable onPress={openEditGearModal}>
+			<View className="p-4 justify-between flex flex-row">
+				<View className="">
+					<H4>{gear.name}</H4>
+					<Text className="text-secondary">{gear.type}</Text>
+				</View>
+				<GearTypeIcon type={gear.type} />
 			</View>
-			<GearTypeIcon type={gear.type} />
-		</View>
+		</Pressable>
 	);
 }
 
@@ -69,11 +74,4 @@ function GearTypeIcon({ type }: { type: GEAR_TYPE }) {
 		default:
 			return <Grinder className="size-12" />;
 	}
-}
-
-interface GearData {
-	name: string;
-	type: GEAR_TYPE;
-	details?: string;
-	settings?: string;
 }
