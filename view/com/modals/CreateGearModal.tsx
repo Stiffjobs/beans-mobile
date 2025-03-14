cf38e9d3dd62b97a11cd5050cf1aba54037a57b1
Stiffjobs <charleschen0306@gmail.com>
2025-03-14 22:08:34 +0900

    first version of create gear modal

--- view/com/modals/CreateGearModal.tsx
+++ view/com/modals/CreateGearModal.tsx
@@ -2,18 +2,143 @@ import { View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 import { Button } from '~/components/ui/button';
 import { Label } from '~/components/ui/label';
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '~/components/ui/select';
 import { Separator } from '~/components/ui/separator';
 import { Text } from '~/components/ui/text';
 import { useModalControls } from '~/state/modals';
+import { WindowOverlay } from '../util/WindowOverlay';
+import { PortalHost } from '@rn-primitives/portal';
+import { GEAR_TYPE } from '~/lib/constants';
+import { z } from 'zod';
+import { createGearSchema } from '~/lib/schemas';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { H4 } from '~/components/ui/typography';
+import { Input } from '~/components/ui/input';
+import { useCreateGear } from '~/state/queries/gears';
+import { useCallback } from 'react';
+import { ErrorMessage } from '~/components/ErrorMessage';
 
 export const snapPoints = ['fullscreen'];
+const SELECT_PORTAL_HOST = 'select-gear-portal';
+type FormFields = z.infer<typeof createGearSchema>;
 export function Component() {
+	const form = useForm<FormFields>({
+		resolver: zodResolver(createGearSchema),
+	});
+	const createGearMutation = useCreateGear();
+	const handleSubmit = useCallback(
+		async (values: FormFields) => {
+			await createGearMutation.mutateAsync(values);
+		},
+		[createGearMutation]
+	);
 	return (
 		<>
 			<Header />
-			<KeyboardAwareScrollView contentContainerClassName="p-4">
-				<Label>Create Gear</Label>
+			<KeyboardAwareScrollView contentContainerClassName="p-4 gap-4">
+				<H4>Create Gear</H4>
+				<Separator />
+				<View className="gap-4">
+					<Controller
+						control={form.control}
+						name="type"
+						render={({ field }) => {
+							return (
+								<>
+									<Label>Type</Label>
+									<Select onValueChange={opt => field.onChange(opt?.value)}>
+										<SelectTrigger>
+											<SelectValue
+												className="text-foreground"
+												placeholder="Select gear type"
+											/>
+										</SelectTrigger>
+										<SelectContent portalHost={SELECT_PORTAL_HOST}>
+											<SelectGroup>
+												{Object.entries(GEAR_TYPE).map(([key, value]) => (
+													<SelectItem key={key} value={value} label={key}>
+														{key}
+													</SelectItem>
+												))}
+											</SelectGroup>
+										</SelectContent>
+									</Select>
+									{form.formState.errors.type && (
+										<ErrorMessage
+											message={form.formState.errors.type?.message}
+										/>
+									)}
+								</>
+							);
+						}}
+					/>
+					<Controller
+						control={form.control}
+						name="name"
+						render={({ field }) => {
+							return (
+								<>
+									<Label>Name</Label>
+									<Input onChangeText={field.onChange} {...field} />
+									{form.formState.errors.name && (
+										<ErrorMessage
+											message={form.formState.errors.name?.message}
+										/>
+									)}
+								</>
+							);
+						}}
+					/>
+					<Controller
+						control={form.control}
+						name="settings"
+						render={({ field }) => {
+							return (
+								<>
+									<Label>Settings</Label>
+									<Input onChangeText={field.onChange} {...field} />
+									{form.formState.errors.settings && (
+										<ErrorMessage
+											message={form.formState.errors.settings?.message}
+										/>
+									)}
+								</>
+							);
+						}}
+					/>
+					<Controller
+						control={form.control}
+						name="details"
+						render={({ field }) => {
+							return (
+								<>
+									<Label>Details</Label>
+									<Input onChangeText={field.onChange} {...field} />
+									{form.formState.errors.details && (
+										<ErrorMessage
+											message={form.formState.errors.details?.message}
+										/>
+									)}
+								</>
+							);
+						}}
+					/>
+				</View>
+				<Button onPress={form.handleSubmit(handleSubmit)}>
+					<Text>Submit</Text>
+				</Button>
 			</KeyboardAwareScrollView>
+			<WindowOverlay>
+				<PortalHost name={SELECT_PORTAL_HOST} />
+			</WindowOverlay>
 		</>
 	);
 }
@@ -26,9 +151,7 @@ function Header() {
 			<Button size={'sm'} variant={'destructive'} onPress={closeModal}>
 				<Text>Cancel</Text>
 			</Button>
-			<Button size={'sm'} variant={'secondary'}>
-				<Text>Create</Text>
-			</Button>
+			<View />
 		</View>
 	);
 }
