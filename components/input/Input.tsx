20f3b434dcf246de859a22f4b51ed00778685c69
Stiffjobs <charleschen0306@gmail.com>
2025-04-06 15:33:33 +0900

    Refactor Input component to support transparent styling and remove BottomSheetInput
    Update the Input component to include a transparent prop for styling flexibility. Remove the BottomSheetInput component, consolidating input functionality into a single Input component for improved maintainability.

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -1,48 +1,38 @@
-import { BottomSheetTextInput } from '@gorhom/bottom-sheet';
-import { BottomSheetTextInputProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetTextInput';
 import React from 'react';
 import { TextInput, TextInputProps } from 'react-native';
 import { cn } from '~/lib/utils';
 
 const Input = React.forwardRef<
 	React.ElementRef<typeof TextInput>,
-	TextInputProps & { editableShowPrimary?: boolean }
->(({ className, placeholderClassName, editableShowPrimary, ...props }, ref) => {
-	return (
-		<TextInput
-			ref={ref}
-			className={cn(
-				'p-3 rounded-md border border-input bg-background  text-foreground',
-				props.editable === false && !editableShowPrimary && 'opacity-50',
-				className
-			)}
-			style={{
-				fontSize: 16,
-			}}
-			placeholderClassName={cn('text-muted-foreground', placeholderClassName)}
-			{...props}
-		/>
-	);
-});
-
-export function BottomSheetInput({
-	className,
-	placeholderClassName,
-	...props
-}: BottomSheetTextInputProps) {
-	return (
-		<BottomSheetTextInput
-			placeholderClassName={cn('text-muted-foreground', placeholderClassName)}
-			className={cn(
-				'p-3 rounded-md border border-input bg-background  text-foreground',
-				className
-			)}
-			style={{
-				fontSize: 16,
-			}}
-			{...props}
-		/>
-	);
-}
+	TextInputProps & { editableShowPrimary?: boolean; transparent?: boolean }
+>(
+	(
+		{
+			className,
+			placeholderClassName,
+			editableShowPrimary,
+			transparent,
+			...props
+		},
+		ref
+	) => {
+		return (
+			<TextInput
+				ref={ref}
+				className={cn(
+					'p-3 rounded-md border border-input bg-background  text-foreground',
+					props.editable === false && !editableShowPrimary && 'opacity-50',
+					transparent && 'bg-transparent border-0 p-0',
+					className
+				)}
+				style={{
+					fontSize: 16,
+				}}
+				placeholderClassName={cn('text-muted-foreground', placeholderClassName)}
+				{...props}
+			/>
+		);
+	}
+);
 
 export { Input };
